a310489523d1bab181258cce228218bf80ad33f1
jorgeboris <eduardo.borisq@gmail.com>
2022-05-15 02:30:52 -0500

    Agregando controlador getEmailStudents y su prueba

--- test/controllers/StudentController.test.js
+++ test/controllers/StudentController.test.js
@@ -1,10 +1,14 @@
 const Reader = require("./../../lib/utils/Reader");
 const StudentController = require("./../../lib/controllers/StudentController");
 
-describe("Test para ExplorerController", () => {
+describe("Test para StudentController", () => {
     test("Verificar los students", () => {
         const students = Reader.readJsonFile("students.json");
         expect(StudentController.getAllStudents(students).length).toBeGreaterThanOrEqual(0);
     });
 
+    test("Obtener Emails de Students", () => {
+        const emailsStudents = StudentController.getEmailStudents(true);
+        expect(emailsStudents).toContain("Sexton@visualpartnership.xyz");
+    });
 });
